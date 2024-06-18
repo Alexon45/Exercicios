8e3a373638dd28ec03581c05bcca5a3a4d3daa2d
Alexon45 <126976486+Alexon45@users.noreply.github.com>
2024-06-18 16:12:58 -0400

    Update Main.kt

--- Main.kt
+++ Main.kt
@@ -2,60 +2,74 @@ import java.math.BigDecimal
 
 fun main() {
 
-   // As notas consideradas são de 100, 50, 20, 10, 5, 2. As moedas possíveis são de 1, 0.50, 0.25, 0.10, 0.05 e 0.01
+    // As notas consideradas são de 100, 50, 20, 10, 5, 2. As moedas possíveis são de 1, 0.50, 0.25, 0.10, 0.05 e 0.01
 
     var saque: BigDecimal = readLine()!!.toBigDecimal()
 
     var notas = arrayOf(0,0,0,0,0,0)
     var moedas = arrayOf(0,0,0,0,0,0)
 
-    while (saque>=100.00){
+    val cemreais:BigDecimal = 100.toBigDecimal()
+    val cinquentareais: BigDecimal = 50.toBigDecimal()
+    val vintereais: BigDecimal = 20.toBigDecimal()
+    val dezreais: BigDecimal = 10.toBigDecimal()
+    val cincoreais: BigDecimal = 5.toBigDecimal()
+    val doisreais: BigDecimal = 2.toBigDecimal()
+
+    val umreal: BigDecimal = 1.toBigDecimal()
+    val cinquentacentavos: BigDecimal = 0.5.toBigDecimal()
+    val vintecincocentavos: BigDecimal = 0.25.toBigDecimal()
+    val dezcentavos: BigDecimal = 0.10.toBigDecimal()
+    val cincocentavos: BigDecimal = 0.05.toBigDecimal()
+    val umcentavo: BigDecimal = 0.01.toBigDecimal()
+
+    while (saque>=cemreais){
         notas[0] = notas[0]+1
-        saque -= 100
+        saque -= cemreais
     }
-    while (saque>=50){
+    while (saque>=cinquentareais){
         notas[1] = notas[1] +1
-        saque -= 50
+        saque -= cinquentareais
     }
-    while (saque>=20){
+    while (saque>=vintereais){
         notas[2] = notas[2]+1
-        saque -= 20
+        saque -= vintereais
     }
-    while (saque>=10){
+    while (saque>=dezreais){
         notas[3] = notas[3]+1
-        saque -=10
+        saque -=dezreais
     }
-    while (saque>=5){
+    while (saque>=cincoreais){
         notas[4] = notas[4]+1
-        saque -=5
+        saque -=cincoreais
     }
-    while (saque>=2){
+    while (saque>=doisreais){
         notas[5] = notas[5]+1
-        saque -=2
+        saque -=doisreais
     }
-    while (saque>=1){
+    while (saque>=umreal){
         moedas[0] = moedas[0]+1
-        saque -=1
+        saque -=umreal
     }
-    while (saque>=0.5){
+    while (saque>=cinquentacentavos){
         moedas[1] = moedas[1]+1
-        saque -=0.5
+        saque -=cinquentacentavos
     }
-    while (saque>=0.25){
+    while (saque>=vintecincocentavos){
         moedas[2] = moedas[2]+1
-        saque -=0.25
+        saque -=vintecincocentavos
     }
-    while (saque>=0.10){
+    while (saque>=dezcentavos){
         moedas[3] = moedas[3]+1
-        saque -=0.10
+        saque -=dezcentavos
     }
-    while (saque>=0.05){
+    while (saque>=cincocentavos){
         moedas[4] = moedas[4]+1
-        saque -=0.05
+        saque -=cincocentavos
     }
-    while (saque>=0.01){
+    while (saque>=umcentavo){
         moedas[5] = moedas[5]+1
-        saque -=0.01
+        saque -=umcentavo
     }
 
     println("NOTAS")
@@ -72,11 +86,5 @@ fun main() {
     println("${moedas[3]} moeda(s) de R$ 0.10")
     println("${moedas[4]} moeda(s) de R$ 0.05")
     println("${moedas[5]} moeda(s) de R$ 0.01")
-
-
-
-
-}
-
-
+    
 }
